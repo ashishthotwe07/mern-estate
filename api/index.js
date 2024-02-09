3d044d3406eb2a768a69951cd42847e2a2125760
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-02-09 07:26:02 +0530

    connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to monogo");
+  })
+  .catch(() => {
+    console.log("error connecting mongo");
+  });
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-    }
-)
+  console.log("Server is running on port 3000!");
+});
