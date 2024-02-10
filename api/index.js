0b7f2c9d1aab1045f7f98fc9de409765d7301cc0
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-02-10 18:39:41 +0530

    created update user api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRoute from "./routes/user.routes.js";
 
@@ -15,7 +16,7 @@ mongoose
     console.log("error connecting mongo");
   });
 const app = express();
-
+app.use(cookieParser())
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
