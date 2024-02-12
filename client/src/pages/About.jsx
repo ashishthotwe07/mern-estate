266fceb56f6a31897e671bed45be163ec2cdc017
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-02-12 14:44:24 +0530

    compelete home page and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Welcome to Realty Rover
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Discover a new standard of excellence in real estate with Realty Rover.
+        As industry trailblazers, we're not just about transactions; we're about
+        transforming lives. With a firm commitment to integrity, innovation, and
+        personalized service, we redefine what it means to buy, sell, or rent a
+        property.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Realty Rover, we're more than just agents; we're partners on your
+        journey to finding your dream home or investment opportunity. Our
+        seasoned professionals combine market expertise with genuine care to
+        guide you through every step of the process. Whether you're a first-time
+        buyer, seasoned investor, or homeowner in transition, we have the
+        resources and dedication to meet your unique needs.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Driven by a passion for excellence and a relentless pursuit of client
+        satisfaction, Realty Rover stands apart as a beacon of trust and
+        reliability. Our mission is simple: to empower you to make informed
+        decisions and achieve your real estate goals with confidence. With
+        Realty Rover, you're not just a client; you're part of our family, and
+        we're committed to your success every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Experience the Realty Rover difference today and embark on a journey of
+        unparalleled service, expertise, and results. Let us be your guide as
+        you navigate the exciting world of real estate and turn your dreams into
+        reality.
+      </p>
+    </div>
+  );
 }
