79ab883b7104ed1537d1d107858354ec1cae9e0c
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-02-12 14:51:28 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRoute from "./routes/user.routes.js";
 import ListingRouter from "./routes/listing.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -16,8 +17,10 @@ mongoose
   .catch(() => {
     console.log("error connecting mongo");
   });
+
+const __dirname = path.resolve();
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
@@ -25,7 +28,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRouter);
-app.use('/api/listing', ListingRouter);
+app.use("/api/listing", ListingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
